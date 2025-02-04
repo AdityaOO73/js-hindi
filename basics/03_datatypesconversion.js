575ab680a5653ad8ad6ccda66a44d93fc6617cf7
Aditya Roy <138622344+AdityaOO73@users.noreply.github.com>
2025-02-04 14:03:37 +0000

    03 added

--- basics/03_datatypesconversion.js
+++ basics/03_datatypesconversion.js
@@ -1,2 +1,16 @@
-let score = 53
+let score = "53abc"
+//let score =
+
 console.log(typeof score)
+let valueInNumber =Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33"=> 33
+//"33abc"=>NaN
+// true =>1; false=>0
+
+let isLoggedIn ="Aditya" // "",
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
