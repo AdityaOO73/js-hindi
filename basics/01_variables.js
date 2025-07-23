01965cf6be52ec6d9346161c51a7a48009ceffaa
Aditya Roy <138622344+AdityaOO73@users.noreply.github.com>
2025-07-23 23:07:08 +0530

    Update 01_variables.js

--- basics/01_variables.js
+++ basics/01_variables.js
@@ -1,4 +1,5 @@
 const accnId = 142563
+let name= "Aditya Roy"
 let accnEmail = "aditya01@gmail.com" // sirf let hi use karenge
 var accnPassword = "123456" // isko hum use nahi karenge 
 accnCity = "hooghly"
@@ -7,6 +8,7 @@ let accnstate;
 //accnId = 2 // not allowed
 
 accnEmail = "raj01@gmail.com"
+name="raj prabhakar"
 accnPassword = "654123"
 accnCity= "Seramporee"
 console.log(accnId )
